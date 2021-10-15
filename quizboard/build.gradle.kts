704ca23efcd2d946bc927a5e9e8a316b88f396c5
ulrikah <ulrikhalmoy@gmail.com>
2021-10-15 17:12:19 +0200

    Basic server for quizboard

--- quizboard/build.gradle.kts
+++ quizboard/build.gradle.kts
@@ -1,7 +1,9 @@
+val kotlinVersion = "1.5.31"
+val ktorVersion = "1.6.4"
 val junitJupiterVersion = "5.8.1"
 
-
 plugins {
+    application
     kotlin("jvm") version "1.5.31"
 }
 
@@ -10,10 +12,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://packages.confluent.io/maven/")
+    maven("https://jitpack.io")
+}
+
+application {
+    mainClass.set("QuizboardKt")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.6")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")

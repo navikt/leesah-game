6986af02fe71a699cce70c82b89a6597d0936811
ulrikah <ulrikhalmoy@gmail.com>
2021-10-19 21:01:20 +0200

    Oppsett for test i Quizboard

---
+++ quizboard/backend/src/test/kotlin/no/nav/quizboard/QuizboardTest.kt
@@ -0,0 +1,61 @@
+package no.nav.quizboard
+
+import no.nav.common.KafkaEnvironment
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.junit.jupiter.api.*
+import java.util.*
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class QuizboardTest {
+
+    private val testTopic = "testTopic"
+    private lateinit var kafkaProducer: Producer<String, String>
+    private lateinit var kafkaConsumer: Consumer<String, String>
+    private val embeddedKafkaEnvironment = KafkaEnvironment(
+        autoStart = false,
+        noOfBrokers = 1,
+        topicInfos = listOf(testTopic).map { KafkaEnvironment.TopicInfo(it, partitions = 1) },
+        withSchemaRegistry = false,
+        withSecurity = false
+    )
+
+    @BeforeAll
+    internal fun setup() {
+        embeddedKafkaEnvironment.start()
+        kafkaProducer = KafkaProducer(producerProperties(), StringSerializer(), StringSerializer())
+        kafkaConsumer = KafkaConsumer(consumerProperties(), StringDeserializer(), StringDeserializer())
+        kafkaConsumer.subscribe(listOf(testTopic))
+    }
+    @AfterAll
+    internal fun teardown() {
+        kafkaConsumer.close()
+        kafkaProducer.close()
+        embeddedKafkaEnvironment.tearDown()
+    }
+
+    @Test
+    fun `tester en test`(){
+        assert(true)
+    }
+
+    private fun producerProperties() =
+        Properties().apply {
+            put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaEnvironment.brokersURL)
+        }
+
+    private fun consumerProperties(): MutableMap<String, Any> {
+        return HashMap<String, Any>().apply {
+            put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaEnvironment.brokersURL)
+            put(ConsumerConfig.GROUP_ID_CONFIG, "integration-test" + UUID.randomUUID().toString().slice(1..5))
+            put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+        }
+    }
+}

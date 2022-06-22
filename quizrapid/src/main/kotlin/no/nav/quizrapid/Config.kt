500d5bdac470524c8658b08349eb1d5bc1d43d2f
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-06-22 10:58:42 +0200

    Ikke autocommit offset

--- quizrapid/src/main/kotlin/no/nav/quizrapid/Config.kt
+++ quizrapid/src/main/kotlin/no/nav/quizrapid/Config.kt
@@ -10,17 +10,22 @@ class Config(
     val appName: String,
     val bootstrapServers: List<String>,
     val quizTopic: String,
+    private val consumerGroup: String,
+    private val autoCommit: Boolean,
     private val kafkaTrustStorePath: String?,
     private val kafkaKeyStorePath: String?,
     private val credStorePassword: String?
 ) {
 
     companion object {
+        val appName = System.getenv("NAIS_APP_NAME")
         fun fromEnv(): Config {
             return Config(
-                System.getenv("NAIS_APP_NAME"),
+                appName,
                 System.getenv("KAFKA_BROKERS").split(";"),
                 System.getenv("QUIZ_TOPIC") ?: "quiz-rapid",
+                System.getenv("QUIZRAPID_CONSUMER_GROUP") ?: "consumer-$appName-v1",
+                System.getenv("QUIZRAPID_AUTO_COMMIT").toBoolean(),
                 System.getenv("KAFKA_TRUSTSTORE_PATH"),
                 System.getenv("KAFKA_KEYSTORE_PATH"),
                 System.getenv("KAFKA_CREDSTORE_PASSWORD")
@@ -28,15 +33,15 @@ class Config(
         }
 
         fun local(appName: String, bootstrapServers: List<String>, quizTopic: String,) =
-            Config(appName, bootstrapServers, quizTopic,null, null, null)
+            Config(appName, bootstrapServers, quizTopic, "local-$appName-cg",false, null, null, null)
     }
 
     internal fun consumerConfig(clientId: String) = Properties().apply {
         put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-        put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-$appName")
+        put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup)
         put(ConsumerConfig.CLIENT_ID_CONFIG, "consumer-$appName-$clientId")
         put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-        put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+        put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit.toString())
         put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1")
         //put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "$maxPollIntervalMs")
         if (kafkaKeyStorePath != null) {

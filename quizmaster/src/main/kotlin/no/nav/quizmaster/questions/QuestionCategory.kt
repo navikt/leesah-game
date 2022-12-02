d19f0ecc5a7b3cd1f420836d33eeb6fb6d76d605
taniaholst <tania.holst@nav.no>
2022-12-02 14:02:00 +0100

    :art: endret navn fra min-max-int til min-max

--- quizmaster/src/main/kotlin/no/nav/quizmaster/questions/QuestionCategory.kt
+++ quizmaster/src/main/kotlin/no/nav/quizmaster/questions/QuestionCategory.kt
@@ -27,7 +27,7 @@ abstract class QuestionCategory(
         if (answer.questionId !in sentQuestions.map { it.messageId }) return false
         answerCounter++
         check(answer)
-//        println(answer)
+        println(answer)
         return true
     }
 

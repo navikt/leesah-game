f8b14695d66d7804c7d9391a6d2de946a2984972
vetleh <vetle.hollund@nav.no>
2023-01-16 11:05:43 +0100

    :bug: Typing bug på ny nav spørsmål fasit

--- quizmaster/src/main/kotlin/no/nav/quizmaster/questions/NAV.kt
+++ quizmaster/src/main/kotlin/no/nav/quizmaster/questions/NAV.kt
@@ -9,7 +9,7 @@ class NAV(private val frequency: Duration, active: Boolean = false) : QuestionCa
     private var nextQuestionAt = LocalDateTime.now()
     private val fasit = mutableMapOf<String, List<String>>()
     private var questionIndex = 0
-    private val navQuestions = mapOf(
+    private val navQuestions:Map<String,List<String>> = mapOf(
         Pair(
             "På hvilken nettside finner man informasjon om rekruttering til NAV IT?",
             listOf("detsombetyrnoe")
@@ -25,7 +25,7 @@ class NAV(private val frequency: Duration, active: Boolean = false) : QuestionCa
         ),
         Pair(
             "Hva heter NAV-direktøren?",
-            "Hans Christian Holte"
+            listOf("Hans Christian Holte")
         )
     )
 
@@ -78,7 +78,7 @@ class NAV(private val frequency: Duration, active: Boolean = false) : QuestionCa
 
 
     override fun sync(question: Question): Boolean {
-        val fasit = navQuestions[question.question]
+        val fasit:List<String>? = navQuestions[question.question]
         if(fasit != null) {
             storeFasit(question, fasit)
             return true

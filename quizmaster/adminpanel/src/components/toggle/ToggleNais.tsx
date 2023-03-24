d1589edc714aea78431c75a4bc819ecc9c992ce9
Tania Holst <taniaholst@Tanias-MacBook-Pro.local>
2023-03-24 15:04:49 +0100

    :sparkles: nais toggle på quizmaster admin

--- quizmaster/adminpanel/src/components/toggle/ToggleNais.tsx
+++ quizmaster/adminpanel/src/components/toggle/ToggleNais.tsx
@@ -2,23 +2,18 @@ import './ToggleNais.scss';
 import React from 'react';
 // @ts-ignore
 import NaisLogo from '../../ikoner/naislogo.svg';
+import { Button } from '@navikt/ds-react';
+import classNames from 'classnames';
 
 interface ToggleNaisProps {
-    setToggle: () => void;
+    setToggle: (toggle: boolean) => void;
     toggle: boolean;
 }
 
 export const ToggleNais = ({ setToggle, toggle }: ToggleNaisProps) => {
     return (
-        <>
+        <Button onClick={() => setToggle(!toggle)} className={classNames('toggleBtn', toggle ? 'toggleActive' : null)}>
             <img src={NaisLogo} alt="Nais Logo" className="naislogo" />
-            <div className="toggle-switch">
-                <div className="button r" id="button-1">
-                    <input type="checkbox" className="checkbox" onClick={setToggle} checked={toggle} />
-                    <span className="knobs" />
-                    <span className="layer" />
-                </div>
-            </div>
-        </>
+        </Button>
     );
 };

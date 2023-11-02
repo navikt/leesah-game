d87c385a47e112ff4c3915dff325a9bb469a3057
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2023-11-02 14:43:07 +0100

    🐛 Enklere sjekk ingress

--- quizmaster/src/main/kotlin/no/nav/quizmaster/questions/NaisIngress.kt
+++ quizmaster/src/main/kotlin/no/nav/quizmaster/questions/NaisIngress.kt
@@ -1,38 +1,11 @@
 package no.nav.quizmaster.questions
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import kotlinx.coroutines.runBlocking
 import no.nav.quizrapid.Answer
 import no.nav.quizrapid.Question
 
 class NaisIngress(maxCount: Int = 1, active: Boolean = true): QuestionCategory("nais-ingress", maxCount, active) {
     override fun check(answer: Answer) {
-        var ingress = answer.answer
-        if (!ingress.startsWith("https://")) {
-            ingress = "https://$ingress"
-        }
-        if (ingress.matches(("\\.intern\\.dev\\.nav\\.no\\/?$").toRegex())) {
-            false.publish(answer.teamName, answer.questionId, answer.messageId)
-            return
-        }
-        val client = HttpClient(CIO) {
-            install(HttpTimeout) {
-                requestTimeoutMillis = 5000
-            }
-        }
-        var statusCode: Int
-
-        runBlocking {
-            val response: HttpResponse = client.get("https://ktor.io/docs/welcome.html")
-            statusCode = response.status.value
-        }
-
-        (statusCode == 200).publish(answer.teamName, answer.questionId, answer.messageId)
-
+        (answer.answer.matches(("\\.intern\\.dev\\.nav\\.no\\/?$").toRegex()).publish(answer.teamName, answer.questionId, answer.messageId))
     }
 
     override fun newQuestions(): List<Question> {

eaa9287477ebe5ac7dda3f45bd4e6e67893c1fd6
Helene Arnesen <helene.arnesen@nav.no>
2023-11-07 13:58:37 +0100

    🐛 make ingress

--- quizmaster/src/main/kotlin/no/nav/quizmaster/questions/NaisIngress.kt
+++ quizmaster/src/main/kotlin/no/nav/quizmaster/questions/NaisIngress.kt
@@ -1,11 +1,37 @@
 package no.nav.quizmaster.questions
 
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.runBlocking
 import no.nav.quizrapid.Answer
 import no.nav.quizrapid.Question
 
 class NaisIngress(maxCount: Int = 1, active: Boolean = true): QuestionCategory("nais-ingress", maxCount, active) {
     override fun check(answer: Answer) {
-        (("\\.intern\\.dev\\.nav\\.no\\/?$").toRegex().containsMatchIn(answer.answer)).publish(answer.teamName, answer.questionId, answer.messageId)
+        var statusCode: Int
+        val hostUrl = answer.answer
+        val security = hostUrl.split(":").first()
+        if (!answer.answer.contains(answer.teamName)
+            || !(("\\.intern\\.dev\\.nav\\.no\\/?$").toRegex().containsMatchIn(answer.answer))
+            || security == "https" ) {
+            false.publish(answer.teamName, answer.questionId, answer.messageId)
+        }
+
+        runBlocking {
+            try {
+                logger.info("henter response")
+                val response: HttpResponse = HttpClient().use {
+                    logger.info("http clienten: $it")
+                    it.get(hostUrl)
+                }
+                statusCode = response.status.value
+                (statusCode == 200).publish(answer.teamName, answer.questionId, answer.messageId)
+            } catch (e: Exception){
+                logger.info("kaster exception")
+                throw e
+            }
+        }
     }
 
     override fun newQuestions(): List<Question> {

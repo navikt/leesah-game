4c8e5c5235a804f3aacf53310d73c6a2876faee8
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-10-09 17:57:17 +0200

    Start of something great

--- doks/astro.config.mjs
+++ doks/astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import mdx from '@astrojs/mdx';
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [mdx()]
-});
+export default defineConfig({});

bf86cd482817f7e9f21ca78931becf373fd3fbc8
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-21 13:29:07 +0100

    Is this it?

--- doks/astro.config.mjs
+++ doks/astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    build: {
+        assetsPrefix: 'https://cdn.nav.no/leesah-quiz/leesah-produktside'
+    }
 });

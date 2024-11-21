d79f17893dc73d7a61a25d03e04e88cd17a8ffdd
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-21 13:57:14 +0100

    Revert

--- doks/astro.config.mjs
+++ doks/astro.config.mjs
@@ -1,14 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
-    adapter: node({
-        mode: "standalone",
-    }),
     build: {
-        assetsPrefix: 'https://cdn.nav.no/leesah-quiz/leesah-produktside'
+        assetsPrefix: 'https://cdn.nav.no/leesah-quiz/leesah-produktside/_astro'
     }
 });

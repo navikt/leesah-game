9e8e856c6e96b5503bfc9defe4aab5168bba4576
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-21 14:34:52 +0100

    Legg til skikkelig nais deploy

--- doks/astro.config.mjs
+++ doks/astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
-    build: {
-        assetsPrefix: 'https://cdn.nav.no/leesah-quiz/leesah-produktside/_astro'
-    }
+    output: 'server',
+    adapter: node({
+        mode: "standalone",
+    }),
+    // build: {
+    //     assetsPrefix: 'https://cdn.nav.no/leesah-quiz/leesah-produktside/_astro'
+    // }
 });

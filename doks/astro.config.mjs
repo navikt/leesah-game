acc85d27bc19cc889c63ee621b268769eec84aeb
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-21 13:52:28 +0100

    Legge til server adapter

--- doks/astro.config.mjs
+++ doks/astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'hybrid',
+    adapter: node({
+        mode: "standalone",
+    }),
     build: {
         assetsPrefix: 'https://cdn.nav.no/leesah-quiz/leesah-produktside'
     }

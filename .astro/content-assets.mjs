9c6281d24aad340718a55f86766a07401b6c4370
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-10 12:57:42 +0100

    Legg inn tekster for Helene og emil

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_17MvYz from "../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md";
+import __ASTRO_IMAGE_IMPORT_OvFgU from "../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md";
+export default new Map([["../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md", __ASTRO_IMAGE_IMPORT_17MvYz], ["../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md", __ASTRO_IMAGE_IMPORT_OvFgU]]);
+		

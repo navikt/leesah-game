81b92699a30e9326f8338f1297ee43ae6f1bbe83
Kyrre Havik <kyrre.havik@nav.no>
2025-01-30 15:57:26 +0100

    upkeep: rebuild av assets

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_17MvYz from '../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md';
-import __ASTRO_IMAGE_IMPORT_OvFgU from '../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md';
-export default new Map([["../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md", __ASTRO_IMAGE_IMPORT_17MvYz], ["../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md", __ASTRO_IMAGE_IMPORT_OvFgU]]);
+import __ASTRO_IMAGE_IMPORT_OvFgU from "../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md";
+import __ASTRO_IMAGE_IMPORT_17MvYz from "../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md";
+export default new Map([["../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md", __ASTRO_IMAGE_IMPORT_OvFgU], ["../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md", __ASTRO_IMAGE_IMPORT_17MvYz]]);
 		

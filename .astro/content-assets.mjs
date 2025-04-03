161cf8c3bcd3b53013a867489a937edaba514bee
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-04-03 14:00:12 +0200

    Første utkast på oversetting

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_OvFgU from "../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md";
 import __ASTRO_IMAGE_IMPORT_17MvYz from "../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md";
-export default new Map([["../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md", __ASTRO_IMAGE_IMPORT_OvFgU], ["../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md", __ASTRO_IMAGE_IMPORT_17MvYz]]);
+import __ASTRO_IMAGE_IMPORT_OvFgU from "../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md";
+import __ASTRO_IMAGE_IMPORT_UVXOF from "../../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fen%2Flagregistrering.md";
+export default new Map([["../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Flagregistrering.md", __ASTRO_IMAGE_IMPORT_17MvYz], ["../../assets/deduplisering-feilet.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fdeduplisering.md", __ASTRO_IMAGE_IMPORT_OvFgU], ["../../../assets/lagregistrering-leaderboard.png?astroContentImageFlag=&importer=src%2Fcontent%2Foppgaver%2Fen%2Flagregistrering.md", __ASTRO_IMAGE_IMPORT_UVXOF]]);
 		
---
+++ src/content/oppgaver/en/lagregistrering.md
@@ -0,0 +1,34 @@
+---
+title: team-registration
+---
+
+The first task each team has to solve is the "team-registration" task. The task is solved by publishing an answering a hex code color which will represent your team in `answer`, in addition to a team name in `team_name`. Your team name must be the same throughout the whole game and cannot be changed.
+
+**Example of a task event**
+
+```json
+{
+  "type": "QUESTION",
+  "questionId": "41fe30bd-4050-45cb-80b2-cb2e82ec4b84",
+  "question": "Choose a hex code of at least 6 characters to represent your team. Example: #FFFFFF",
+  "category": "team-registration",
+  "answer_format": "Hex code as string"
+}
+```
+
+**Example of answer**
+```json
+{
+  "type": "ANSWER",
+  "answerId": "d36b4273-0571-42f2-b3bd-7b7987de43b0",
+  "questionId": "41fe30bd-4050-45cb-80b2-cb2e82ec4b84",
+  "category": "team-registration",
+  "team_name": "l33t team",
+  "answer": "#ff2255",
+  "created": "2022-11-07T14:53:27.581147"
+}
+```
+
+When team-registration is approved your team will be displayed at the leaderboard.
+
+![Leaderboard after team-registration is approved](../../../assets/lagregistrering-leaderboard.png)

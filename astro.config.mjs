946f6848c9064b4936a35aef187950ceb7bffc83
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-04-03 13:34:39 +0200

    Skru på oversettinger til engelsk

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+    i18n: {
+        locales: ["nb", "en"],
+        defaultLocale: "nb"
+    },
     output: 'server',
     adapter: node({
         mode: "standalone",
---
+++ src/content/oppgaver/en/aritmetikk.md
@@ -0,0 +1,5 @@
+---
+title: 'arithmetic'
+---
+
+## Arithmetic

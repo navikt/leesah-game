8357eb9ce2cd98e9b3463426be3188657d1ffb9b
Tania Holst <holst.tania@gmail.com>
2026-01-15 10:21:08 +0100

    :art: Gjorde endringer for å gjøre plass til pleesah game

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,18 +9,24 @@ export const showDefaultLang = false;
 export const ui = {
     no: {
         'nav.about': 'Hvem står bak Leesah?',
-        'nav.play': 'Vil du spille Leesah Game?',
         'alt.megaman': 'Megaman som løper',
         'alt.spinning_fox': 'En rødrev som løper i sirkel',
         'alt.sleeping_cat': 'Sovende katt',
-        'alt.leaderboard': 'Illustrasjon over leaderboardet i et spill av leesah',
+        'alt.poddy_gif': 'En papegøye som er utkledd som en pirat',
+        'nav.leesah-play': 'Vil du spille Leesah Game?',
+        'nav.leesah-game': 'Leesah Game',
+        'alt.leesah-leaderboard': 'Illustrasjon over leaderboardet i et spill av leesah',
+        'nav.pleesah-game': 'Pleesah Game',
     },
     en: {
         'nav.about': 'Who is behind Leesah?',
-        'nav.play': 'Do you want to play?',
         'alt.megaman': 'Megaman running',
         'alt.spinning_fox': 'A red fox running in circles',
         'alt.sleeping_cat': 'A cat sleeping',
-        'alt.leaderboard': 'Illustration of the leaderboard in a game by Leesah',
+        'alt.poddy_gif': 'A parrot dressed as a pirate',
+        'nav.leesah-play': 'Do you want to play Leesah Game?',
+        'nav.leesah-game': 'Leesah Game',
+        'alt.leesah-leaderboard': 'Illustration of the leaderboard in a game by Leesah',
+        'nav.pleesah-game': 'Pleesah Game',
     },
 } as const;
